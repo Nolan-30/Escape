52e7c2c17bcd2f52050b30caedd497af1e031ed7
Nolan D'almeida <Nolan@example.com>
2026-02-17 13:52:20 +0100

    Gestion des collisions avec cubes rouges + perte de HP si touché

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,27 +1,48 @@
 using UnityEngine;
+using TMPro; // Utilisation du texte UI
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int hp = 10; // PV
+    public int hp = 10;
+    public TextMeshProUGUI textePV;
 
-    // declenchement de la fonction des que le joueur touche un item
+    void Start()
+    {
+        MettreAJourUI(); // affiche els hp au lancement du jeu
+    }
+
+    // Declenchement de la fonction des que le joueur touche un objet
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // On vérifie si joueur touche bien un "Item"
         if (collision.gameObject.name.Contains("Item"))
         {
-            hp -= 2; // on enelve de hp
-            Debug.Log("Aïe ! PV restants : " + hp);
-
-            // destruction du cube rouge
+            hp -= 2;
+            MettreAJourUI();
             Destroy(collision.gameObject);
-
 
             if (hp <= 0)
             {
-                Debug.Log("GAME OVER !");
-
+                GameOver(); // appl de la fonction
             }
+        }
+    }
+
+    void GameOver()
+    {
+        // changement du txt_HP pr gameover
+        textePV.text = "GAME OVER !";
+
+        // 2. On fige le temps (le jeu s'arrête littéralement)
+        Time.timeScale = 0;
+
+        Debug.Log("Le jeu est arrêté !");
+    }
+    // rafraichir le temps
+    void MettreAJourUI()
+    {
+        if (textePV != null)
+        {
+            textePV.text = "HP : " + hp;
         }
     }
 }
