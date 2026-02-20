db491fe516a0e2d29aa51bcac71e160e6e7845ab
Ethannn91 <ethan.perru@edu.esiee-it.fr>
2026-02-20 03:44:08 +0100

    Save

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // change de scene 
+using UnityEngine.SceneManagement; 
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject menuOptions;
+
     public void Jouer()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OuvrirOptions()
+    {
+        menuOptions.SetActive(true); 
+    }
+
+    public void FermerOptions()
+    {
+        menuOptions.SetActive(false); 
     }
 
     public void Quitter()
     {
         Debug.Log("Le jeu se ferme !");
-        Application.Quit(); // Ferme le jeu 
+        Application.Quit(); 
     }
 }
